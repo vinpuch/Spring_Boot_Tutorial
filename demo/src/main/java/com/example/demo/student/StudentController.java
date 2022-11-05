df979e4ddd021846a340a41578c269ed973b3d62
vinpuch <94136786+vinpuch@users.noreply.github.com>
2022-11-05 14:23:59 +0100

    Buissneslayer erstellt

--- demo/src/main/java/com/example/demo/student/StudentController.java
+++ demo/src/main/java/com/example/demo/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,13 +11,15 @@ import java.util.List;
 @RestController
 @RequestMapping(path="api/v1/student")
 public class StudentController {
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
     @GetMapping
-    public List<Student> getStudent(){
-        return List.of(
-                new Student(1L,
-                        "Mariam",
-                        21, LocalDate.of(2000, Month.JANUARY, 5),
-                        "mariam.jamal@gmail.com")
-        );
+    public List<Student> getStudents(){
+       return studentService.getStudents();
     }
 }
--- demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -1,2 +1,24 @@
-package com.example.demo.student;public class StudentService {
+package com.example.demo.student;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class StudentService {
+    @GetMapping
+    public List<Student> getStudents(){
+
+        return List.of(
+                new Student(1L,
+                        "Mariam",
+                        21, LocalDate.of(2000, Month.JANUARY, 5),
+                        "mariam.jamal@gmail.com")
+        );
+
+    }
 }
