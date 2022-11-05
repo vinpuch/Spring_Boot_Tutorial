a9f5420a8d56492648e8373703961df61ad491a2
vinpuch <94136786+vinpuch@users.noreply.github.com>
2022-11-05 14:02:31 +0100

    neue klasse

--- demo/src/main/java/com/example/demo/student/Student.java
+++ demo/src/main/java/com/example/demo/student/Student.java
@@ -1,4 +1,82 @@
 package com.example.demo.student;
 
+import java.time.LocalDate;
+
 public class Student {
+    private Long id;
+    private String name;
+    private Integer age;
+    private LocalDate dov;
+    private String email;
+
+    public Student() {
+    }
+
+    public Student(Long id, String name, Integer age, LocalDate dov, String email) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.dov = dov;
+        this.email = email;
+    }
+
+    public Student(String name, Integer age, LocalDate dov, String email) {
+        this.name = name;
+        this.age = age;
+        this.dov = dov;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public LocalDate getDov() {
+        return dov;
+    }
+
+    public void setDov(LocalDate dov) {
+        this.dov = dov;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", dov=" + dov +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+
 }
